d16933485785aabf253b80f5ca7baa0da598f9a1
anils7016 <anils7016@gmail.com>
2021-04-01 16:49:01 +0530

    changes display image

--- upload.php
+++ upload.php
@@ -1,12 +1,15 @@
 <?php
+	$curTime = microtime(true);
+   	$timeConsumed = 0;
    	if ( 0 < $_FILES['file']['error'] ) {
         echo 'Error: ' . $_FILES['file']['error'] . '<br>';
     }
     else {
     	$filename = str_replace(" ", "_",$_REQUEST['file2']);
-    	move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/discussionfile/' .$filename);
-
-    	$upload_time = time() - $_SERVER['REQUEST_TIME'];
-    echo $upload_time;
+    	$uploadfile = move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/discussionfile/' .$filename);
+    	if($uploadfile){
+    		$timeConsumed = (microtime(true) - $curTime)*1000;
+		}
+    	echo number_format((float)$timeConsumed, 2, '.', ''); 
     }
 ?>
