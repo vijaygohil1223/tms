85c0cdacf5ab2497955e363009b7e7f8fb0299ed
anils7016 <anils7016@gmail.com>
2021-05-05 18:55:14 +0530

    file upload issue project scoop item

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -3625,6 +3625,7 @@ $scope.dtOptions = DTOptionsBuilder.newOptions().
                 return true; //to allow file submission.
             },
         }); 
+        $('.ajax-upload-dragdrop:eq(1)').hide();
     }, 100);
 
     $scope.addToCopy = function(fid) {
