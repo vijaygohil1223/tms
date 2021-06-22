4d37d3a94b23820ed12c773314a8d0cd27074378
anils7016 <anils7016@gmail.com>
2021-06-22 10:40:17 +0530

    work instruction issue

--- js/app.material.js
+++ js/app.material.js
@@ -546,10 +546,10 @@ app.config(function($locationProvider, $routeProvider, $httpProvider, $compilePr
             templateUrl: modulesPath + 'activation.html',
             controller: 'userActivationController',
             activetab: 'activation-separator'
-        }).when('/work-instruction', {
-            templateUrl: modulesPath + 'work-instruction.html',
+        }).when('/work-instructions', {
+            templateUrl: modulesPath + 'work-instructions.html',
             controller: 'workInstructionsController',
-            activetab: 'work-instruction',
+            activetab: 'work-instructions',
             role: '1'
         }).otherwise({
             redirectTo: '/'
