209f08b6e3cb71111dbdfde92219e3f515a972d2
vijay gohil <vijayg.kanhasoft@gmail.com>
2022-05-10 11:02:05 +0530

    changes in smpt mail

--- api/class/contactpermsg.class.php
+++ api/class/contactpermsg.class.php
@@ -78,8 +78,10 @@ class contactPerMsg {
         $this->_mailer->Host = "ssl://smtp.gmail.com";
         $this->_mailer->SMTPAuth = "true";
         $this->_mailer->Port = "465";
-        $this->_mailer->Username = "ChrisGilesItWorks@gmail.com";
-        $this->_mailer->Password = "Knh@Admin@123";
+        // $this->_mailer->Username = "ChrisGilesItWorks@gmail.com";
+        // $this->_mailer->Password = "Knh@Admin@123";
+        $this->_mailer->Username = SMTP_EMAIL_USER;
+        $this->_mailer->Password = SMTP_EMAIL_PASSWORD;
 
         $this->_mailer->From = "Kanhasoft.com";
         $this->_mailer->FromName = "Kanhasoft";
