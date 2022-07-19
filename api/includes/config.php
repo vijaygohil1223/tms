ea497cf10caa409ca3ff60737a124db4dd1934ac
vijay gohil <vijayg.kanhasoft@gmail.com>
2022-07-19 12:55:04 +0530

    changes in mailjet library function

--- api/includes/config.php
+++ api/includes/config.php
@@ -1,4 +1,5 @@
 <?php
+
 define('SITE_URL', "http://" . $_SERVER['SERVER_NAME'] . "/");
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'] . "/");
 define('UPLOADS_ROOT', DOCUMENT_ROOT . "uploads/");
@@ -16,6 +17,12 @@ define('SMTP_EMAIL_PASSWORD', 'kanha@tmschris');
 define('EMAIL_API_KEY', '3efaa57c4b8abc7e48828126b802720f');
 define('EMAIL_SECRETE_KEY', '7a593f51ca6285bc96d548223414144b');
 
+// mailJET
+define('EMAIL_API_KEY', '3efaa57c4b8abc7e48828126b802720f');
+define('EMAIL_SECRETE_KEY', '7a593f51ca6285bc96d548223414144b');
+define('SMTP_FROM_NAME','TMS');
+define('SMTP_FROM_EMAIL','anil.kanhasoft@gmail.com');
+
 define('DB_PREFIX', 'tms_');
 define('LOGOUT_TIME', 1800);
 define('DECIMAL', 2);
@@ -28,4 +35,5 @@ define('FORMATS', "pdf,docx,doc");
 define('IMG_FORMATS', "jpg,jpeg,gif,png");
 define('DEFAULT_COUNTRY_ID', 'IN');
 date_default_timezone_set('Asia/Kolkata');
+
 ?>
