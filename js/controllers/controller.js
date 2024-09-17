3e15141466dd0f2e39a2a7a1c510cdb971480138
vijaygohil1223 <vijayg.kanhasoft@gmail.com>
2024-09-17 18:44:22 +0530

    client invoice-total selected invoice

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -36494,40 +36494,48 @@ app.controller('loginController', function ($scope, $log, rest, $window, $locati
         });
     }
     $scope.checkedIds = [];
-    $scope.checkInvoiceIds = function(id){
-        //var result = arrayRemove(array, 6);
-        
-        if(id){
-            if(id == 'all'){
+    $scope.checkInvoiceIds = function(id, totalp) {
+        // Initialize total price if not already initialized
+        if (!$scope.totalSelectedPrice) {
+            $scope.totalSelectedPrice = 0;
+        }
+    
+        if (id) {
+           
+            if (id === 'all') {
                 let isCheckedAll = $('#checkAll').is(':checked') ? 'true' : 'false';
-                
-                if(isCheckedAll == 'true'){
+               
+                if (isCheckedAll === 'true') {
                     $("input[id^=invoiceCheck]:checkbox").prop("checked", true);
-                    for (var i = 0; i < angular.element('[id^=invoiceCheck]').length; i++) {
-                        var invoiceselected = $('#invoiceCheck' + i).is(':checked') ? 'true' : 'false';
-                        
-                        if (invoiceselected == 'true') {
-                            var invoiceIds = angular.element('#invoiceCheckData' + i).val();
-                            $scope.checkedIds.push(parseInt(invoiceIds));
+                    angular.forEach($scope.invoiceListAll, function(invoiceL, index) {
+                        if (!invoiceL.credit_note_id) {
+                            // Add invoice cost to total price
+                            $scope.totalSelectedPrice += invoiceL.Invoice_cost;
+                            $scope.checkedIds.push(invoiceL.invoice_id);
+                        } else {
+                            // Subtract invoice cost (as it's a credit note)
+                            $scope.totalSelectedPrice -= invoiceL.Invoice_cost;
                         }
-                    }        
-                }else{
-                    $('input[id^=invoiceCheck]:checkbox').removeAttr('checked');
-                    $('input[id^=checkAll]:checkbox').removeAttr('checked');
-                    //$('#checkAll').removeAttr('checked');
+                    });
+                } else {
+                    $('input[id^=invoiceCheck]:checkbox').prop('checked', false);
+                    $('input[id^=checkAll]:checkbox').prop('checked', false);
                     $scope.checkedIds = [];
+                    $scope.totalSelectedPrice = 0; // Reset total price when all are deselected
                 }
-            }else{
+            } else {
                 let isChecked = $('.invoiceCheck' + id).is(':checked') ? 'true' : 'false';
-                
-                if(isChecked == 'true')
+    
+                if (isChecked === 'true') {
                     $scope.checkedIds.push(id);
-                else
+                    $scope.totalSelectedPrice += totalp; // Add price to total
+                } else {
                     $scope.checkedIds = arrayRemove($scope.checkedIds, id);
-            }        
-        }    
-                        
-    }
+                    $scope.totalSelectedPrice -= totalp; // Subtract price from total
+                }
+            }
+        }
+    };
 
     $scope.dtExcelTable = function(type){
         if(type=='create-date')    
@@ -36874,7 +36882,7 @@ app.controller('loginController', function ($scope, $log, rest, $window, $locati
               var filteredData = table.rows({ filter: 'applied' }).data().toArray();
           
               $scope.totalPrice = 0;
-              
+              $scope.totalSelectedPrice = 0;
               angular.forEach(filteredData, function(item) {
                 // Assuming the 4th column contains the price data
                 var htmlContent = item[4]; // Adjust the column index as needed
@@ -36887,6 +36895,15 @@ app.controller('loginController', function ($scope, $log, rest, $window, $locati
                 if (!isNaN(value)) {
                   $scope.totalPrice += value;
                 }
+
+                // var invoiceId = item[0]; 
+                // // Check if the checkbox for the corresponding row is checked
+                // var isChecked = $('.invoiceCheck' + invoiceId).is(':checked');
+                // // Only add to the total price if the checkbox is checked
+                // if (isChecked && !isNaN(value)) {
+                //     $scope.totalSelectedPrice += value;
+                // }
+
               });
           
               // Update the scope with the new total
