779c5532c24cff14892bcaa9493d423264fd49df
vijaygohil1223 <vijayg.kanhasoft@gmail.com>
2024-10-24 11:11:35 +0530

    Button changes like if url having id then copy button show otherwise not

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -25147,6 +25147,20 @@ app.controller('loginController', function ($scope, $log, rest, $window, $locati
         localStorage.setItem('copyProjectData', JSON.stringify(copyObj))
     }
 
+    $scope.isEditIdUrl = false;
+
+    $scope.$watch(function() {
+        return $location.path();
+    }, function(newPath) {
+        // Check if the URL contains an ID (anything after /general/)
+        var pathParts = newPath.split('/');
+        if (pathParts[1] === 'general' && pathParts[2]) {
+            $scope.isEditIdUrl = true; // URL contains /general/id
+        } else {
+            $scope.isEditIdUrl = false; // URL is only /general
+        }
+    });
+
     $scope.copyProjectData1 = function(orderID){
        
         var copyMessage = "<p> Are you sure you want to copy this project? </p><p> It will create new project as a copy of this project.</p> ";
